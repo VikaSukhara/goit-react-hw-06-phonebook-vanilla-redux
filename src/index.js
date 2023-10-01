8a1752f13706ac3f0e4923f959906c57acd327f5
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-10-01 22:20:33 +0200

    jsn

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import { AppNew } from 'components/AppNew';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from 'components/redux/store';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
     <Provider store = {store}>
     <AppNew />
     </Provider>
-    </BrowserRouter>
   </React.StrictMode>
 );
